85b9d32c51287abf3ba14a941a4c820cf861d0c6
srdjanRakic <srg_ian@hotmail.com>
2017-09-19 07:22:42 +0200

    FIrebase configured.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import FireBase from 'firebase'
+
+let config = {
+  apiKey: "AIzaSyC2abaJk8IJQwhUIO2zgo_wkK8gDCAIS-4",
+  authDomain: "vuejs-books-8c952.firebaseapp.com",
+  databaseURL: "https://vuejs-books-8c952.firebaseio.com",
+  projectId: "vuejs-books-8c952",
+  storageBucket: "vuejs-books-8c952.appspot.com",
+  messagingSenderId: "949868469908"
+}
+
+let app = FireBase.initializeApp(config)
+let db = app.database()
+
+let bookRef = db.ref('books')
 
 export default {
   name: 'app',
-  components: {
-    Hello
+  firabase: {
+    books: bookRef
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueFire from './App'
+
+Vue.use(VueFire)
 
 Vue.config.productionTip = false
 
